e5378c125daa5386e24f46a917e9427904a0c4d7
karinEkenberg <zirkon6@hotmail.com>
2024-01-10 08:26:49 +0100

    last commit

--- Program.cs
+++ Program.cs
@@ -1,18 +1,4 @@
-﻿
-//Pseudo kod
-// Välkomnande meddelande<<<<<<<<<<<
-// En lista för att spara historik för räkningar<<<<<<
-// Användaren matar in tal och matematiska operation<<<<<<
-// OBS! Användaren måsta mata in ett tal för att kunna ta sig vidare i programmet!<<<<<<<<<
-// Ifall användaren skulle dela med 0 visa Ogiltig inmatning!<<<<<<<<<
-// Lägga resultat till listan<<<<<<<
-// Visa resultat<<<<<<<<
-// Fråga användaren om den vill visa tidigare resultat.<<<
-// Visa tidigare resultat<<<<<<<
-// Fråga användaren om den vill avsluta eller fortsätta.<<<<<<<<
-
-
-using System.ComponentModel.Design;
+﻿using System.ComponentModel.Design;
 
 namespace Miniräknaren
 {
